3d3b21ad305c580d54a98a145bbc2896f4d9fc88
coding_jc <996186701@qq.com>
2020-10-20 00:09:33 +0800

    description:增加部门管理页面

--- src/components/sys/basic/DepMana.vue
+++ src/components/sys/basic/DepMana.vue
@@ -1,10 +1,54 @@
 <template>
-    <div>部门管理</div>
+    <div style="width: 500px">
+        <el-input
+                prefix-icon="el-icon-search"
+                placeholder="请输入部门名称搜索"
+                v-model="filterText">
+        </el-input>
+
+        <el-tree
+                :data="deps"
+                :props="defaultProps"
+                :filter-node-method="filterNode"
+                ref="tree">
+        </el-tree>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "DepMana"
+        name: "DepMana",
+        watch: {
+            filterText(val) {
+                this.$refs.tree.filter(val);
+            }
+        },
+        data(){
+            return{
+                filterText: '',
+                deps:[],
+                defaultProps: {
+                    children: 'children',
+                    label: 'name'
+                }
+            }
+        },
+        methods: {
+            initdeps(){
+                this.getRequest("/system/basic/department/").then(resp => {
+                    if (resp) {
+                        this.deps = resp;
+                    }
+                })
+            },
+            filterNode(value, data) {
+                if (!value) return true;
+                return data.name.indexOf(value) !== -1;
+            }
+        },
+        mounted() {
+            this.initdeps();
+        }
     }
 </script>
 
